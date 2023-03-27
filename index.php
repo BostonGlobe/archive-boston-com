31de316f53c5b97ce2431de8d6330bcccc3a1beb
photocurio <sunraydust@gmail.com>
2023-03-27 11:18:21 -0400

    added index, header and footer

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php
+
+include( 'header.html' ); ?>
+
+<div id="articleText">
+	<div id="article">
+		<div class="hideMe">
+			<!--
+	 <headline>Teradyne will shed 532 jobs and cut workers' pay</headline>
+	 <source>Boston Globe</source>
+	 <teasetext>Teradyne Inc. of North Reading is cutting 532 of its 3,800 employees worldwide, and imposing a 10 percent pay cut on the rest. Teradyne employs 1,055 people in Massachusetts, but the company did not say how many local workers will be cut.</teasetext>
+	 <byline>Hiawatha Bray</byline>
+	 <date>January 30, 2009</date>
+ -->
+		</div>
+		<div id="articleHeader">
+			<div id="headTools">
+				<a href="http://www.boston.com/news/globe/"><img
+						src="http://cache.boston.com/bonzai-fba/File-Based_Image_Resource/from_provider_globe.gif"
+						alt="The Boston Globe" title="The Boston Globe" height="20" width="105" border="0"
+						class="providerlogo" align="right">
+					<input type="hidden" name="logotype" value="Globe Story"></a>
+				<h1>Teradyne will shed 532 jobs and cut workers' pay</h1>
+				<h2>Industry pinched by slow gadget demand</h2>
+				<div class="utility">
+					<span id="byline">
+						By
+						<a
+							href="http://search.boston.com/local/Search.do?s.sm.query=Hiawatha+Bray&amp;camp=localsearch:on:byline:art">Hiawatha
+							Bray</a>
+					</span>
+					<div class="cf"></div>
+					<span id="dateline">
+						Globe Staff
+						<span class="listPipe">/</span>
+						January 30, 2009
+					</span>
+					<div class="cf"></div>
+					<div id="tools">
+						<ul>
+							<li id="shareEmail"><a class="etaf"
+									href="javascript:openWindow('http://tools.boston.com/pass-it-on?story_url=http://www.boston.com/business/technology/articles/2009/01/30/teradyne_will_shed_532_jobs_and_cut_workers_pay','mailit','scrollbars,resizable,width=770,height=450');">Email</a><span
+									class="listPipe etafPipe">|</span></li>
+							<li id="sharePrint"><a
+									href="http://www.boston.com/business/technology/articles/2009/01/30/teradyne_will_shed_532_jobs_and_cut_workers_pay?mode=PF">Print</a><span
+									class="listPipe">|</span></li>
+							<li id="shareSingle"><a onclick="switchSingleOn()" href="#">Single Page</a><span
+									class="listPipe">|</span></li>
+							<li id="toolsYahooB">
+								<script showbranding="0" src="http://d.yimg.com/ds/badge.js" badgetype="text">
+								bostoncom751:
+									http: //www.boston.com/business/technology/articles/2009/01/30/teradyne_will_shed_532_jobs_and_cut_workers_pay/
+								</script><span class="listPipe">|</span>
+							</li>
+							<li id="toolsShareThis">
+								<script type="text/javascript"
+									src="http://w.sharethis.com/widget/?tabs=web%2Cpost&amp;charset=utf-8&amp;services=%2Cfacebook%2Cdigg%2Cdelicious%2Creddit%2Cmixx%2Cnewsvine%2Ctechnorati%2Cstumbleupon&amp;style=default&amp;publisher=e1e0ea5a-a326-4731-b1d1-f21623043511">
+								</script>
+							</li>
+						</ul>
+						<span class="articleTextsize">
+							Text size
+							<span class="minus"><span onclick="javascript:fontsizedown();"
+									class="imageLink">–</span></span>
+							<span class="plus"><span onclick="javascript:fontsizeup();"
+									class="imageLink">+</span></span>
+						</span>
+					</div><!-- end tools -->
+
+
+				</div><!-- End utility -->
+			</div><!-- End headTools -->
+		</div><!-- End articleHeader -->
+		<div id="articleGraphs">
+			<div id="page1">
+				<p><a href="http://finance.boston.com/boston?Page=QUOTE&amp;Ticker=TER" target="_new">Teradyne Inc.</a>
+					of North Reading is cutting 532 of its 3,800 employees worldwide, and imposing a 10 percent pay cut
+					on the rest. Teradyne employs 1,055 people in Massachusetts, but the company did not say how many
+					local workers will be cut.</p>
+				<p>Teradyne, the world's leading maker of equipment for testing microchips, doesn't sell its complex,
+					costly products in stores. Instead, they're purchased by major electronics firms around the globe.
+					But the decline in consumer electronics sales that has forced retailers such as Circuit City and
+					Tweeter out of business has also led to production cuts at chip makers like <a
+						href="http://finance.boston.com/boston?Page=QUOTE&amp;Ticker=INTC" target="_new">Intel Corp.</a>
+					and <a href="http://finance.boston.com/boston?Page=QUOTE&amp;Ticker=AMD" target="_new">Advanced
+						Micro Devices Inc.</a> That means a lot fewer orders for the kind of chip manufacturing gear
+					sold by Teradyne.</p>
+				<p>"These are very tough times and these are significant cuts that we're making," said Teradyne chief
+					executive Michael Bradley. The company's chief financial officer, Gregory Beecher, warned that
+					"there is no short-term recovery in sight."</p>
+				<p>The layoffs at Teradyne follow a fourth-quarter loss and slower sales. For the three months ended
+					Dec. 31, Teradyne lost $55.3 million, or 33 cents a share, compared with a profit of $16.7 million,
+					or 10 cents a share, in the same period last year. Revenue declined to $194.8 million from $260.4
+					million. Teradyne also warned that it expected to lose between 31 and 38 cents a share in the first
+					quarter.</p>
+				<p>Another local manufacturer of chip making gear revealed dismal results yesterday. <a
+						href="http://finance.boston.com/boston?Page=QUOTE&amp;Ticker=VSEA" target="_new">Varian
+						Semiconductor Equipment Associates Inc.</a> of Gloucester reported that revenue for its fiscal
+					first quarter ended Jan. 2 was down 58 percent from a year ago. The company went from a $43.7
+					million first-quarter profit last year to a $13.6 million loss this year.</p>
+				<p>"It's the collapse in consumer demand," said Teradyne spokesman Andy Blanchard. "The consumer, who
+					drives about 60 to 70 percent of semiconductor demand, stopped buying in the third quarter."</p>
+				<p>Chip makers have responded with large layoffs. Intel, the world's leading semiconductor company, is
+					laying off up to 6,000 workers. Rival Advanced Micro Devices will cut 1,100 jobs, and <a
+						href="http://finance.boston.com/boston?Page=QUOTE&amp;Ticker=TXN" target="_new">Texas
+						Instruments Inc.</a> plans 1,800 layoffs. At the same time, chip makers are reducing output and
+					canceling plans for new production lines. That means fewer orders for Teradyne and other companies
+					that produce chip making gear.</p>
+				<p>Len Jelinek, director and chief analyst for semiconductor manufacturing at research firm iSuppli
+					Corp., said semiconductor equipment makers such as Teradyne won't recover any time soon. "All these
+					guys are going to suffer significantly in 2009," Jelinek said. "We might see additional factory
+					closures or furloughs or even layoffs."</p>
+				<p>In a recent report, iSuppli predicted that semiconductor equipment purchases for 2008 would amount to
+					$42.7 billion, down 21.1 percent from $54 billion in 2007. Jelinek warned that sales this year could
+					decline another 40 percent from last year's figure. Sales may pick up in 2010, because existing
+					equipment will become obsolete and must be replaced, but Jelinek predicted that any improvement will
+					be slight.</p>
+				<p>John Greenagel, spokesman for the Semiconductor Industry Association in San Jose, Calif., said the
+					only hope for a turnaround lies in a renewal of consumer demand for computers, TVs, digital cameras,
+					and other chip-based products. "Consumers have become the main driver of semiconductor consumption,"
+					Greenagel said. "Restoration in consumer confidence is critical to the turnaround in demand."</p>
+				<p><i>Hiawatha Bray can be reached at <a href="mailto:bray@globe.com">bray@globe.com</a>.</i><img
+						class="storyend"
+						src="http://cache.boston.com/bonzai-fba/File-Based_Image_Resource/dingbat_story_end_icon.gif"
+						width="6" height="8" border="0" alt=""></p>
+				<div class="copyright">© Copyright 2009 Globe Newspaper Company.</div>
+			</div>
+		</div>
+	</div>
+	<div class="cf"></div>
+	<a name="commentAnchor"></a>
+	<div id="articleComments">
+		<div id="aComments"></div>
+	</div>
+	<div class="hideMe">
+		<div id="relatedContent">
+			<div id="relatedBox" class="relatedBox">
+				<ul class="linklist">
+					<li><span class="leadIn">Gallery </span><a
+							href="http://www.boston.com/jobs/galleries/mass_layoffs_2008/" class="bold">Recent major
+							layoffs in Massachusetts</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include( 'footer.html' ); ?>
